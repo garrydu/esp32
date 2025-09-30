c5db6efbbaaac19aaf576316c73fe8c069d54705
garrydu <garrydu17sep2021@gmail.com>
2025-09-30 02:12:44 +0000

    recreate the sample files

--- esp32-http-server/.pio/build/esp32/FrameworkArduino/FirmwareMSC.cpp.d
+++
@@ -1,5 +0,0 @@
-.pio/build/esp32/FrameworkArduino/FirmwareMSC.cpp.o: \
- C:/Users/GarryDu/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.cpp \
- C:/Users/GarryDu/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.h \
- C:/Users/GarryDu/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
- C:/Users/GarryDu/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
